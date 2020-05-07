529b7e07a324603eacdaaa7ba0e3e7278478a957
James Hagood <cardfaux@tutanota.com>
2020-05-07 09:58:44 -0500

    updated again edited cofig gile

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,42 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const { data } = await graphql(`
-    query {
-      posts: allContentfulPost {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-      work: allContentfulWork {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
-  data.posts.edges.forEach(({ node }) => {
-    createPage({
-      path: `blog/${node.slug}`,
-      component: path.resolve("./src/templates/blog-template.js"),
-      context: {
-        slug: node.slug,
-      },
-    })
-  })
-  data.work.edges.forEach(({ node }) => {
-    createPage({
-      path: `work/${node.slug}`,
-      component: path.resolve("./src/templates/work-template.js"),
-      context: {
-        slug: node.slug,
-      },
-    })
-  })
-}
+//   const { data } = await graphql(`
+//     query {
+//       posts: allContentfulPost {
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//       work: allContentfulWork {
+//         edges {
+//           node {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   data.posts.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `blog/${node.slug}`,
+//       component: path.resolve("./src/templates/blog-template.js"),
+//       context: {
+//         slug: node.slug,
+//       },
+//     })
+//   })
+//   data.work.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `work/${node.slug}`,
+//       component: path.resolve("./src/templates/work-template.js"),
+//       context: {
+//         slug: node.slug,
+//       },
+//     })
+//   })
+// }
